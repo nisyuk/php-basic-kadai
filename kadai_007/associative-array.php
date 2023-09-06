08edcff24d9af5ca94722aab036a47e67e30ed88
nisyuk <140573790+nisyuk@users.noreply.github.com>
2023-09-06 16:21:03 +0900

    Add files via upload
    kadai_007をアップロードしました。

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>連想配列</title>
+</head>
+
+<body>
+  <?php
+  // 連想配列に値を入れる
+  $vegetable_data = ['name' => 'onion', 'price' => '200', 'weight' => '160'];
+
+  // 連想配列を出力
+  print_r($vegetable_data);
+
+?>
+</body>
+
+</html>
